8ba29dcaa2cc7be6c94a166ef3f00cb235582b84
Rodrigo Barbosa <rodrigo.dsb93@gmail.com>
2020-07-31 20:40:48 -0300

    atualização do cadastro

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import PageDefault from '../../../componentes/PageDefault';
-import FormField from '../../../componentes/FormField';
+import PageDefault from '../../../componentes/PageDefault/index';
+import FormField from '../../../componentes/FormField/index';
 
 function CadastroCategoria() {
   const valoresIniciais = {
@@ -9,7 +9,6 @@ function CadastroCategoria() {
     descricao: '',
     cor: '',
   }
-  
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
 
@@ -29,12 +28,28 @@ function CadastroCategoria() {
     );
   }
 
+  useEffect(() => {
+    if(window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias'; 
+      fetch(URL)
+       .then(async (respostaDoServer) =>{
+        if(respostaDoServer.ok) {
+          const resposta = await respostaDoServer.json();
+          setCategorias(resposta);
+          return; 
+        }
+        throw new Error('Não foi possível pegar os dados');
+       })
+    }    
+  }, []);
+
   return (
     <PageDefault>
       <h1>Cadastro de Categoria: {values.nome}</h1>
 
       <form onSubmit={function handleSubmit(infosDoEvento) {
           infosDoEvento.preventDefault();
+
           setCategorias([
             ...categorias,
             values
@@ -58,7 +73,7 @@ function CadastroCategoria() {
           value={values.descricao}
           onChange={handleChange}
         />
-
+  
         <FormField
           label="Cor"
           type="color"
@@ -66,17 +81,18 @@ function CadastroCategoria() {
           value={values.cor}
           onChange={handleChange}
         />
-
+  
         <button>
           Cadastrar
         </button>
       </form>
+      
 
       <ul>
         {categorias.map((categoria, indice) => {
           return (
             <li key={`${categoria}${indice}`}>
-              {categoria.nome}
+              {categoria.titulo}
             </li>
           )
         })}
