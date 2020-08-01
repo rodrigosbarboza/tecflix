44804934e6250c0047dcbb2a1f50ada5d7e05d34
Rodrigo Barbosa <rodrigo.dsb93@gmail.com>
2020-08-01 18:01:29 -0300

    Update de correção

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,54 +1,77 @@
-import React from 'react';
-import styled from 'styled-components';
-import Menu from '../../componentes/Menu/index';
-import dadosInicial from '../../data/dados_iniciais.json';
+/* eslint-disable no-console */
+import React, { useEffect, useState } from 'react';
+// import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../componentes/BannerMain';
 import Carousel from '../../componentes/Carousel';
-import Footer from '../../componentes/Footer/index';
-
-const AppWrapper = styled.div`
-  background: var(--grayDark);
-}`;
+import PageDefault from '../../componentes/PageDefault';
+import categoriasRepository from '../../repositories/categorias';
 
 function Home() {
+  const [dadosIniciais, setDadosIniciais] = useState([]);
+
+  useEffect(() => {
+    // http://localhost:8080/categorias?_embed=videos
+    categoriasRepository.getAllWithVideos()
+      .then((categoriasComVideos) => {
+        console.log(categoriasComVideos[0].videos[0]);
+        setDadosIniciais(categoriasComVideos);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
-    <AppWrapper style={{backgrou: "#141414"}}>
-      <Menu />
+    <PageDefault paddingAll={0}>
+      {dadosIniciais.length === 0 && (<div>Loading...</div>)}
 
-      <BannerMain
-        videoTitle={dadosInicial.categorias[0].videos.titulo}
-        url={dadosInicial.categorias[0].videos[0].url}
-        videoDescription={"Tendências para tenologia 2020 - Estudos, movimentações do mercado e novas legislaturas indicam que o mundo da tecnologia e inovação irá seguir por quatro caminhos neste novo ano: celulares com telas dobráveis; expansão da rede de dados 5G; implementação da Lei Geral de Proteção aos Dados Pessoais (LGPD) no Brasil; e o crescimento da utilização de assistentes por voz. Cada qual à sua maneira, essas tendências devem movimentar o ambiente econômico e social das empresas de tecnologia no ano de 2020, criando novas formas de se conectar com o mundo."}
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription={dadosIniciais[0].videos[0].description}
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais[0]}
+              />
+            </div>
+          );
+        }
+
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
+
+      {/* <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription="O que"
       />
-
       <Carousel
         ignoreFirstVideo
-        category={dadosInicial.categorias[0]}
+        category={dadosIniciais.categorias[0]}
       />
-
       <Carousel
-        category={dadosInicial.categorias[1]}
+        category={dadosIniciais.categorias[1]}
       />
-
       <Carousel
-        category={dadosInicial.categorias[2]}
+        category={dadosIniciais.categorias[2]}
       />
-
       <Carousel
-        category={dadosInicial.categorias[3]}
+        category={dadosIniciais.categorias[3]}
       />
-
       <Carousel
-        category={dadosInicial.categorias[4]}
-      />
-
-      <Carousel
-        category={dadosInicial.categorias[5]}
-      />
-
-      <Footer />
-
-    </AppWrapper>
+        category={dadosIniciais.categorias[4]}
+      /> */}
+    </PageDefault>
   );
 }
 
